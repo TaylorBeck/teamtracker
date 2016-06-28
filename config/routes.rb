cfb21c1088ef9ed76dd3f00f38e4d043c8ca5557
Martha Rodgers & Mandy McDorman & Elliot Wolfe <pair+mrodgers25.whatthemandy.wolfy858@devbootcamp.com>
2016-06-27 19:40:45 -0700

    Fixing merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,9 @@
 Rails.application.routes.draw do
-  get 'sports/index'
-
-  get 'sports/new'
-
-  get 'sports/create'
-
-  get 'sports/show'
-
-  get 'sports/edit'
-
-  get 'sports/update'
-
-  get 'sports/destroy'
-
   get '/register' => 'users#register'
   get '/login' => 'users#login'
   post '/users' => 'users#create'
 
   resources :teams
-
+  resources :players
   resources :sports
-
 end
