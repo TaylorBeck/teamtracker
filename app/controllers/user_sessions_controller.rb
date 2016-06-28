efcd36dd27c0855e029d0ec2185331c094a11aa1
Michael Dumalag & Taylor Beck <pair+michaeldumalag.taylorbeck@devbootcamp.com>
2016-06-27 19:45:13 -0700

    Finish user authentication with sorcery

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,20 @@
+class UserSessionsController < ApplicationController
+
+  # GET
+  def new
+  end
+
+  def create
+    if login(params[:user_sessions][:email], params[:user_sessions][:password])
+      redirect_back_or_to(teams_path)
+    else
+      render action: :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to :root
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,17 +17,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET
-  def login
-    render 'users/login'
-  end
-
-  # # POST
-  # def session
-  #   @user = User.find()
-  #   # session[:user_id] = User.find()
-  # end
-
   private
 
   def user_params
