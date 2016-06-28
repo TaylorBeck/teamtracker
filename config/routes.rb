90126cfbb946fb87d8e18d1109e419e13b3e01ba
[01;31m[K"name"[m[K: "Martha Rodgers <[01;31m[K"email"[m[K: "mrodgers25@gmail.com>
2016-06-27 19:27:14 -0700

    completed games model, migration and controller setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'games/index'
+
+  get 'games/new'
+
+  get 'games/create'
+
+  get 'games/show'
+
+  get 'games/edit'
+
+  get 'games/update'
+
+  get 'games/destroy'
+
   get '/register' => 'users#register'
   get '/login' => 'users#login'
   post '/users' => 'users#create'
