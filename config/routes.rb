c52ab64a52380f247a60f4121975539251fd9f3c
Michael Dumalag & Taylor Beck <pair+michaeldumalag.taylorbeck@devbootcamp.com>
2016-06-27 19:55:42 -0700

    Finish user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'teams#index'
+
   get '/register' => 'users#register'
-  get '/login' => 'users#login'
+  get '/login' => 'user_sessions#new'
+  get '/logout' => 'user_sessions#destroy'
   post '/users' => 'users#create'
+
+  resources :user_sessions, only: [:new, :create, :destroy]
 
   resources :teams
 end
