b525db3945e5ec7d70932dff3ed646acce178ff8
[01;31m[K"name"[m[K: "Taylor Beck <[01;31m[K"email"[m[K: "taylorgbeck@gmail.com>
2016-06-29 12:42:04 -0700

    Move response text action to rsvp controller

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -10,6 +10,60 @@ class RsvpsController < ApplicationController
     #twilio logic. Buttons on game show page
   end
 
+  # TWILIO sends a post request here
+  def receive_response_from_player
+
+
+    # Find the player by the phone number that sent the text
+    player_who_sent_message = Player.find_by(phone_number: params['From'])
+    
+    # Get the game from the user-specified game ID
+    id_user_sent = params['Body'].split[" "].last.to_i
+    game = Game.find_by(id: id_user_sent)
+
+    # Get the Rsvp between the game and the player
+    rsvp = Rsvp.where(player: player_who_sent_message, game: game)
+
+    if rsvp
+      if params['Body'].downcase == 'yes'
+        #set response to yes
+
+        rsvp.update(response: "yes")
+
+        response = Twilio::TwiML::Response.new do |r|
+          r.Message "You have confirmed that you WILL be going to the game."
+        end
+
+        render :xml => response.to_xml
+
+      elsif params['Body'].downcase == 'no'
+        #set response to no
+
+        rsvp.update('no')
+
+        response = Twilio::TwiML::Response.new do |r|
+          r.Message "You have confirmed that you WILL NOT be going to the game."
+        end
+
+        render :xml => response.to_xml
+      else
+        #invalid text received
+        response = Twilio::TwiML::Response.new do |r|
+          r.Message "Sorry, I'm a dumb robot and couldn't understand that. Please respond with either 'yes #{game.id}' or 'no #{game.id}'."
+        end
+
+        render :xml => response.to_xml
+      end
+    else
+      # There is no Rsvp for this player
+      response = Twilio::TwiML::Response.new do |r|
+        r.Message "Hmmm... I can't seem to find a game that you are playing in. Please contact your coach for help."
+      end
+
+      render :xml => response.to_xml
+    end
+  end
+
   private
 
   def response_params
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,10 +2,15 @@ class Rsvp < ActiveRecord::Base
   # Set default value of response to be 'not responded'
   before_save :default_values
 
+  after_save :send_reminder_automatically
+
   validates :responder, :game, :response, presence: true
 
   belongs_to :responder, class_name: "Player"
   belongs_to :game
+
+  # DISABLE 3rd party POST request detection
+  skip_before_filter :verify_authenticity_token
 
   def default_values
     self.response ||= "not responded"
@@ -51,56 +56,6 @@ class Rsvp < ActiveRecord::Base
       to: player_phone,
       body: "You have a game on #{game_date} at #{game_time}. #{team_name} is playing at #{location}. Respond 'yes #{self.game.id}' or 'no #{self.game.id}' to let your coach know if you are going."
       )
-  end
-
-  def receive_response_from_player
-    # Find the player by the phone number that sent the text
-    player_who_sent_message = Player.find_by(phone_number: params['From'])
-    
-    # Get the game from the user-specified game ID
-    id_user_sent = params['Body'].split[" "].last.to_i
-    game = Game.find_by(id: id_user_sent)
-
-    # Get the Rsvp between the game and the player
-    rsvp = Rsvp.where(player: player_who_sent_message, game: game)
-
-    if rsvp
-      if params['Body'].downcase == 'yes'
-        #set response to yes
-        self.response = "yes"
-
-        response = Twilio::TwiML::Response.new do |r|
-          r.Message "You have confirmed that you WILL be going to the game."
-        end
-
-        render :xml => response.to_xml
-
-      elsif params['Body'].downcase == 'no'
-        #set response to no
-        self.response = "no"
-
-        response = Twilio::TwiML::Response.new do |r|
-          r.Message "You have confirmed that you WILL NOT be going to the game."
-        end
-
-        render :xml => response.to_xml
-      else
-        #invalid text received
-        response = Twilio::TwiML::Response.new do |r|
-          r.Message "Sorry, I'm a dumb robot and couldn't understand that. Please respond with either 'yes #{game.id}' or 'no #{game.id}'."
-        end
-
-        render :xml => response.to_xml
-      end
-    else
-      # There is no Rsvp for this player
-      response = Twilio::TwiML::Response.new do |r|
-        r.Message "Hmmm... I can't seem to find a game that you are playing in. Please contact your coach for help."
-      end
-
-      render :xml => response.to_xml
-    end
-
   end
 
   # Create new delayed jobs to send reminders automatically
