fd6bc182f878bbbf2a30e0e283a09fe6ddcb1a7a
Michael Dumalag & Elliot Wolfe <pair+michaeldumalag.wolfy858@devbootcamp.com>
2016-06-28 22:09:15 -0700

    Fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,16 @@ Team.delete_all
 Player.delete_all
 Sport.delete_all
 
-Game.create(date: DateTime.now, time: DateTime.now, location: 'North Clairemont Rec Center')
-Game.create(date: DateTime.now, time: DateTime.now, location: 'South Clairemont Rec Center')
-
 
 User.create(name: 'Gary', phone_number: '123-456-7890', email: 'email@gmail.com', password: 'pass')
 
 
-Team.create(name: 'Garys Gilligans', sport_id: 1, ideal_players: 6, min_players: 4, min_females: 2, manager_id: 1)
-Team.create(name: 'Garys Gophers', sport_id: 1, ideal_players: 4, min_players: 2, min_females: 1, manager_id: 1)
+t1 = Team.create(name: "Gary's Gilligans", sport_id: 1, ideal_players: 6, min_players: 4, min_females: 2, manager_id: 1)
+t2 = Team.create(name: "Gary's Gophers", sport_id: 1, ideal_players: 4, min_players: 2, min_females: 1, manager_id: 1)
+
+Game.create(team: t1, date: DateTime.now, time: DateTime.now, location: 'North Clairemont Rec Center')
+Game.create(team: t2, date: DateTime.now, time: DateTime.now, location: 'South Clairemont Rec Center')
+
 
 
 Player.create(name: 'Joe', gender: 'm', phone_number: '122-344-5678')
