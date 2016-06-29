3f6ab43471530372a6be3931a10833370180c298
[01;31m[K"name"[m[K: "Martha Rodgers <[01;31m[K"email"[m[K: "mrodgers25@gmail.com>
2016-06-29 13:27:07 -0700

    game page logic and css fixes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -11,7 +11,7 @@ class PlayersController < ApplicationController
   def create
     @player = Player.new(player_params)
     if @player.save
-      redirect_to teams_path
+      redirect_to manage_path
     else
       render "new"
     end
