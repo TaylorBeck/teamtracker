d572a1a0f37522d37104d99e8adc9ffe5c5918bf
[01;31m[K"name"[m[K: "Taylor Beck <[01;31m[K"email"[m[K: "taylorgbeck@gmail.com>
2016-06-29 12:24:48 -0700

    Add manual messages to rsvp model

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -11,6 +11,28 @@ class Rsvp < ActiveRecord::Base
     self.response ||= "not responded"
   end
 
+  # Manager clicks button to send a reminder
+  def send_reminder_manually
+    boot_twilio
+
+    player_phone = self.responder.phone_number
+    date = self.game.date
+    # Get date as 'Wednesday, June 2nd'
+    game_date = date.strftime("%A, %b #{date.day.ordinalize}")
+    # Get time as '5:30PM'
+    game_time = self.game.time.strftime("%l:%M%p").lstrip
+    team_name = self.game.team.name
+    location = self.game.location
+
+    # Send a text message to the player
+    @client.messages.create(
+      from: @twilio_number,
+      to: player_phone,
+      body: "You have a game on #{game_date} at #{game_time}. #{team_name} is playing at #{location}. Respond 'yes #{self.game.id}' or 'no #{self.game.id}' to let your coach know if you are going."
+      )
+  end
+
+  # Automatic reminder texts sent out 7, 3, and 2 days before the game
   def send_reminder_automatically
     boot_twilio
 
@@ -82,9 +104,9 @@ class Rsvp < ActiveRecord::Base
   end
 
   # Create new delayed jobs to send reminders automatically
-  handle_asynchronously :send_reminder_automatically, :run_at => Proc.new { 7.days.from_now }
-  handle_asynchronously :send_reminder_automatically, :run_at => Proc.new { 3.days.from_now }
-  handle_asynchronously :send_reminder_automatically, :run_at => Proc.new { 2.days.from_now }
+  handle_asynchronously :send_reminder_automatically, :run_at => Proc.new { self.game.date - 7.days }
+  handle_asynchronously :send_reminder_automatically, :run_at => Proc.new { self.game.date - 3.days }
+  handle_asynchronously :send_reminder_automatically, :run_at => Proc.new { self.game.date - 2.days }
 
   # Configure Twilio
   def boot_twilio
